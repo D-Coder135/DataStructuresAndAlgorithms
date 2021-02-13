b49db6800d847f2a98197197652735992b7e6d9e
dbc2201 <divyanshb22@gmail.com>
2021-02-13 18:54:22 +0530

    implement the methods from the ListADT to the DoublyLinkedList class

--- src/datastructures/linear/lists/linked/doubly/DoublyLinkedList.java
+++ src/datastructures/linear/lists/linked/doubly/DoublyLinkedList.java
@@ -1,4 +1,20 @@
 package datastructures.linear.lists.linked.doubly;
 
-public class DoublyLinkedList<E> {
+import datastructures.linear.lists.ListADT;
+
+public class DoublyLinkedList<E> implements ListADT<E> {
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
 }
