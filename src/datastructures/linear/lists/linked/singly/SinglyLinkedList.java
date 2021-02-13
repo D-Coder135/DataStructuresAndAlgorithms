64690ab845337dc484d2b46ccbba91e5e4bc2aad
dbc2201 <divyanshb22@gmail.com>
2021-02-13 18:40:33 +0530

    implement the methods of the ListADT in the SinglyLinkedList class; revert jdk version to 15.0.1

--- src/datastructures/linear/lists/linked/singly/SinglyLinkedList.java
+++ src/datastructures/linear/lists/linked/singly/SinglyLinkedList.java
@@ -1,4 +1,20 @@
 package datastructures.linear.lists.linked.singly;
 
-public class SinglyLinkedList<E> {
+import datastructures.linear.lists.ListADT;
+
+public class SinglyLinkedList<E> implements ListADT<E> {
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
 }
