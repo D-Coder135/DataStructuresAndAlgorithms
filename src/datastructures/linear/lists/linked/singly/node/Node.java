824042369b71c63f5666f2159dd23facfe5f4db8
dbc2201 <divyanshb22@gmail.com>
2021-02-13 13:32:42 +0530

    create a constructor for the node class of a singly linked list, the constructor only requires the data of the node

--- src/datastructures/linear/lists/linked/singly/node/Node.java
+++ src/datastructures/linear/lists/linked/singly/node/Node.java
@@ -8,7 +8,7 @@ public class Node<E> {
      * 3. Pre-Defined "Data" classes in the JDK.
      * 4. "Data" Classes created by a programmer in a package.
      * */
-    private E data;
+    private final E data;
 
     /*
      * This variable will store the reference to the next node of the current node.
@@ -16,5 +16,14 @@ public class Node<E> {
      * It is hence possible to query the current node for the next node, and thus we can traverse to the next node if
      * required.
      * */
-    private Node<E> next;
+    private final Node<E> next;
+
+    /*
+     * This constructor is used in the case that we want to create a node but only the data of the node is
+     * available to us, in this case, the node simply DOES NOT have a next node, and thus the value is set to null.
+     * */
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
 }
