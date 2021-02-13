1827a1334087bb2a3925befd243e90fe882eb839
dbc2201 <divyanshb22@gmail.com>
2021-02-13 13:21:24 +0530

    add JavaDoc for ListADT

--- src/datastructures/linear/lists/ListADT.java
+++ src/datastructures/linear/lists/ListADT.java
@@ -1,5 +1,8 @@
 package datastructures.linear.lists;
 
+/**
+ * This ListADT interface will serve as the ADT (Abstract Data Type) for a List Data Structure we want to implement.
+ */
 public interface ListADT<E> {
 
 }
