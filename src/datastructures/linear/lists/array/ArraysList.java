fb25b642cd41e315b2f457ec4b324d102a8ea68a
dbc2201 <divyanshb22@gmail.com>
2021-02-13 18:59:12 +0530

    implement methods from the ListADT

--- src/datastructures/linear/lists/array/ArraysList.java
+++ src/datastructures/linear/lists/array/ArraysList.java
@@ -1,4 +1,20 @@
 package datastructures.linear.lists.array;
 
-public class ArraysList<E> {
+import datastructures.linear.lists.ListADT;
+
+public class ArraysList<E> implements ListADT<E> {
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
 }
