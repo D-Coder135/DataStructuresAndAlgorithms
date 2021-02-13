7cca738e0d4e94ede091a3305620922607204b11
dbc2201 <divyanshb22@gmail.com>
2021-02-13 13:27:04 +0530

    create a method in the List ADT to 1. remove the last element of the list 2. remove an element at a specified index

--- src/datastructures/linear/lists/ListADT.java
+++ src/datastructures/linear/lists/ListADT.java
@@ -12,4 +12,22 @@ public interface ListADT<E> {
      */
     boolean add(E item);
 
+    /**
+     * This method will remove an existing element from our list.
+     * Since no parameter to the method is allowed it simply removes the last element from our list.
+     *
+     * @return it returns the value of the item that was just removed.
+     */
+    public abstract E remove();
+
+    /**
+     * This method is an overloaded version of the previous remove() method we had built.
+     * Since lists allow random access, we can specify an integer type index to the parameter to delete
+     * the item on that particular index.
+     *
+     * @return it returns the value of the item that was just removed from the index.
+     * @throws IndexOutOfBoundsException throws an exception if the specified index is out of bounds.
+     */
+    public abstract E remove(int index) throws IndexOutOfBoundsException;
+
 }
