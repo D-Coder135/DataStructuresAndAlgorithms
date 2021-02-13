ccc23f1058c050d4ea33fa8cc211e5d2ef06f7ac
dbc2201 <divyanshb22@gmail.com>
2021-02-13 18:55:03 +0530

    create the class fields for a DoublyLinkedList

--- src/datastructures/linear/lists/linked/doubly/DoublyLinkedList.java
+++ src/datastructures/linear/lists/linked/doubly/DoublyLinkedList.java
@@ -1,8 +1,14 @@
 package datastructures.linear.lists.linked.doubly;
 
 import datastructures.linear.lists.ListADT;
+import datastructures.linear.lists.linked.doubly.node.Node;
 
 public class DoublyLinkedList<E> implements ListADT<E> {
+
+    private final Node<E> head = null;
+    private final Node<E> tail = null;
+    private final int size = 0;
+
     @Override
     public boolean add(E item) {
         return false;
