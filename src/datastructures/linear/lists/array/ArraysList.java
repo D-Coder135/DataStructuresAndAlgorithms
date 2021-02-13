9c6c0efe78bd6f8e58714e22e92ca188da7276ad
dbc2201 <divyanshb22@gmail.com>
2021-02-13 19:00:48 +0530

    add fields and constructor for the ArraysList class

--- src/datastructures/linear/lists/array/ArraysList.java
+++ src/datastructures/linear/lists/array/ArraysList.java
@@ -3,6 +3,15 @@ package datastructures.linear.lists.array;
 import datastructures.linear.lists.ListADT;
 
 public class ArraysList<E> implements ListADT<E> {
+
+    private final int MAX_SIZE;
+    private E[] array;
+    private int currentSize = 0;
+
+    public ArraysList(int maxSize) {
+        this.MAX_SIZE = maxSize;
+    }
+
     @Override
     public boolean add(E item) {
         return false;
