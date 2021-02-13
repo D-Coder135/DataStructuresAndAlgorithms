59051fee6df0b33316c0dc937ff9ab5e60d6ee2c
dbc2201 <divyanshb22@gmail.com>
2021-02-13 13:29:20 +0530

    create the data field for the node of a singly linked list

--- src/datastructures/linear/lists/linked/singly/node/Node.java
+++ src/datastructures/linear/lists/linked/singly/node/Node.java
@@ -1,5 +1,12 @@
 package datastructures.linear.lists.linked.singly.node;
 
 public class Node<E> {
-
+    /*
+     * The E type here specifies that the data variable can be of any reference type, including but not limited to -
+     * 1. Wrapper Types for primitive data types in the JDK.
+     * 2. String, StringBuilder/StringBuffer, BigInteger, BigDecimal.
+     * 3. Pre-Defined "Data" classes in the JDK.
+     * 4. "Data" Classes created by a programmer in a package.
+     * */
+    private E data;
 }
