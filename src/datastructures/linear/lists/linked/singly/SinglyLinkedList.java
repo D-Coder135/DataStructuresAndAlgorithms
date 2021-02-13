651d44629244f349e43d4ce64241802f6bc724a4
dbc2201 <divyanshb22@gmail.com>
2021-02-13 19:05:22 +0530

    add a private method to get a node based off of an index in the linked list; though the linked list do not posses any indices for their nodes, we are processing the nodes based off of logical indices.

--- src/datastructures/linear/lists/linked/singly/SinglyLinkedList.java
+++ src/datastructures/linear/lists/linked/singly/SinglyLinkedList.java
@@ -31,4 +31,26 @@ public class SinglyLinkedList<E> implements ListADT<E> {
     public E remove(int index) throws IndexOutOfBoundsException {
         return null;
     }
+
+    /**
+     * This method will return a node at the specified index from the linked list.
+     *
+     * @param index An non-negative index from the linked list.
+     * @return The node at the specified index.
+     */
+    private Node<E> getNode(int index) {
+        Node<E> currentNode = head;
+
+        /*
+         * using a loop to traverse the whole linked list,
+         * the loop will run until i is less than the size of the linked list,
+         * AND
+         * the current node traversed is not equal to null.
+         * */
+        for (int i = 0; i < index; i++) {
+            // change the reference value of the response variable to the next node
+            currentNode = currentNode.getNext();
+        }
+        return currentNode;
+    }
 }
